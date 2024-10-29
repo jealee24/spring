719c06c8e31690c593f9d411a2c390af16027441
성선후 <jealee24@naver.com>
2024-10-29 13:49:08 +0900

    커밋 테스트4

--- SpringSecPrj3/src/main/java/com/newlecture/webprj/service/MemberShipService.java
+++ SpringSecPrj3/src/main/java/com/newlecture/webprj/service/MemberShipService.java
@@ -5,5 +5,6 @@ import java.sql.SQLException;
 import com.newlecture.webprj.vo.Notice;
 
 public interface MemberShipService {
+	// 커밋테스트4
 	public void insertAndPointUPofMember(Notice n, String uid) throws ClassNotFoundException, SQLException;
 }
